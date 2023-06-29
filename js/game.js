14079b7e4a85001014cda1421a57cfcad7cdab39
Hamed Jalili <itjalili82@gmail.com>
2023-06-29 19:34:01 +0200

    update

--- js/game.js
+++ js/game.js
@@ -1,72 +1,69 @@
-
 class Game {
-    constructor () {
-        this.startScreen = document.getElementById('game-intro');
-        this.gameScreen = document.getElementById('game-screen');
-        this.gameEndScreen = document.getElementById('game-end');
-        this.player = new Player(
-            this.gameScreen,
-            200,
-            500,
-            100,
-            150,
-            "./images/car.png"
-          );
-        this.height = 600;
-        this.width = 500;
-        this.obstacles = [new Obstacle(this.gameScreen)];
-        this.score = 0;
-        this.lives = 3;
-        this.gameIsOver = false;
-}
-
-start () {
-    this.gameScreen.style.height = this.height + 'px';
-    this.gameScreen.style.width = this.width + 'px';
-
-    this.startScreen.style.display = "none";
-    
-    this.gameScreen.style.display = "block";
-
-    this.gameLoop();
-}
-
-gameLoop () {
-        if (this.gameIsOver) {
-            return;
-       }
-
-    this.update();
-
-    if (Math.random() > 0.98 && this.obstacles.length < 1 {
-        this.obstacles.push(new Obstacle(this.gameScreen))
+    constructor() {
+      this.startScreen = document.getElementById('game-intro')
+      this.gameScreen = document.getElementById('game-screen')
+      this.gameEndScreen = document.getElementById('game-end')
+      this.height = 600
+      this.width = 500
+      this.player = new Player(this.gameScreen)
+      this.obstacles = []
+      this.isGameOver = false
+      this.score = 0
+      this.lives = 3
+      this.animateId
     }
-
-    window.requestAnimationFrame(() => {
-        this.gameLoop();
-    }) 
-}
-
-update () {
-    this.player.move();
-    const obstaclesToKeep = [];
-    this.obstacles.forEach(obstacle => {
+  
+    start() {
+      this.gameScreen.style.width = `${this.width}px`
+      this.gameScreen.style.height = `${this.height}px`
+  
+      this.startScreen.style.display = 'none'
+      this.gameScreen.style.display = 'block'
+  
+      this.gameLoop()
+    }
+  
+    gameLoop() {
+      this.update()
+      console.log(this.animateId)
+      if (this.animateId % 200 === 0) {
+        this.obstacles.push(new Obstacle(this.gameScreen))
+      }
+      if (this.isGameOver) {
+        this.endGame()
+      } else {
+        this.animateId = requestAnimationFrame(() => this.gameLoop())
+      }
+    }
+  
+    update() {
+      this.player.move()
+      const obstaclesToKeep = []
+      this.obstacles.forEach(obstacle => {
         obstacle.move()
-        if (this.player.didCollide(obstacle)){
-            console.log('collision')
-            obstacle.element.remove(); 
+        if (this.player.didCollide(obstacle)) {
+          obstacle.element.remove()
+          this.lives -= 1
         } else if (obstacle.top > this.gameScreen.offsetHeight) {
-            console.log('out of screen')
+          this.score += 1
         } else {
-            obstaclesToKeep.push(obstacle)
+          obstaclesToKeep.push(obstacle)
         }
-        // this.gameIsOver = 
-    })
-    this.obstacles = obstaclesToKeep;
-    
-}
-
-
-
-
-}
+      })
+      this.obstacles = obstaclesToKeep
+  
+      if (this.lives <= 0) {
+        this.isGameOver = true
+      }
+    }
+  
+    endGame() {
+      this.player.element.remove()
+      this.obstacles.forEach(obstacle => obstacle.element.remove())
+  
+      // Hide game screen
+      this.gameScreen.style.display = 'none'
+      // Show end game screen
+      this.gameEndScreen.style.display = 'block'
+    }
+  }
